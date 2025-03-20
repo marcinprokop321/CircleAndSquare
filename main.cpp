ad7a2d209afb1edcaa11c9c36053dcabaf9e6d32
marcinprokop <61902544+marcinprokop321@users.noreply.github.com>
2025-03-20 15:36:36 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <time.h>
+#include <cstdlib>
+struct punkt {
+    double x, y;
+};
+int main()
+{
+    srand(time(NULL));
+    double size = 0;
+    std::cout << "Enter square size: ";
+    std::cin >> size;
+    int kount = 0;
+    int ile_razy = 0;
+    std::cout << "How many times?: ";
+    std::cin >> ile_razy;
+    for (int x = 0;x < ile_razy;x++)
+    {
+        double iks = (((rand() % 10000) / 10000.0f) * size);
+        double igrek = (((rand() % 10000) / 10000.0f)*size);
+
+        if ((size / 2.0f)* (size / 2.0f) > abs((iks - (size / 2.0f)) * (iks - (size / 2.0f))) + abs((igrek - (size / 2.0f)) * (igrek - (size / 2.0f))))
+            kount++;
+
+    }
+    std::cout << "!!! trafiono " << kount << " razy!!!";
+}
